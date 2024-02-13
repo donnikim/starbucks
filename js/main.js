154ed9caf365be78d1f1a8131d20f70e869188c0
donnikim <dongil0913@gmail.com>
2024-02-13 22:14:37 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click',function(){
-  searchInputEl.focus();
-});
-// 클릭하였을 때 클래스를 생성 시킨다.
-// 플레이스 홀더에 통합검색 넣는다
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
-});
-//클릭하였을 떄 클래스를 지워줌
-//
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl= document.querySelector('#to-top');
@@ -148,6 +131,3 @@ spyEls.forEach(function(spyEl){
     .setClassToggle(spyEl,'show')
     .addTo(new ScrollMagic.Controller());
 });
-//현재 년도
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
